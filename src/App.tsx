fe0e2dc0d36875ec66d0dfa35f5f5dc0f31045a7
금정욱 <gold@geumjeong-ugs-MacBook-Pro.local>
2025-05-06 23:55:04 +0900

    Switch to HashRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, useNavigate, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 import './App.css';
 
 // 카카오 로그인 페이지 컴포넌트
